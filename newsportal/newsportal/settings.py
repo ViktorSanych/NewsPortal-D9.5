ea664bcc09061147944cd0565b05f216afd0161e
ViktorSanych <i_vitek@mail.ru>
2023-05-17 08:17:33 +0300

    Итоговое задание D9.5 изменения после оценки

--- newsportal/newsportal/settings.py
+++ newsportal/newsportal/settings.py
@@ -165,6 +165,6 @@ load_dotenv()
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'exampleforSF'
-EMAIL_HOST_PASSWORD = 'pvsghphvzrtmmzla'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('email_host_password')
 EMAIL_USE_SSL = True
